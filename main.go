4c744e97abd694c1e840c0f821decf6eb4f3e13e
Sebastiaan Gerritsen <s.e.gerritsen@outlook.com>
2024-05-11 19:18:43 +0200

    Update reference to conform to Terraform specification

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 		// TODO: Update this string with the published name of your provider.
 		// Also update the tfplugindocs generate command to either remove the
 		// -provider-name flag or set its value to the updated provider name.
-		Address: "registry.terraform.io/hashicorp/bitwarden-secrets",
+		Address: "registry.terraform.io/hashicorp/terraform-provider-bitwarden-secrets",
 		Debug:   debug,
 	}
 
